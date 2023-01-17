f216d1d498b81da2c187fa5d11d2ee01089f26be
cameliabenavides10 <cameliabenavides@gmail.com>
2023-01-16 18:09:49 -0600

    added current weather, fixed cards and appended data for forecast

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,13 @@
-var fiveDayForecastEl = $('#fiveDay-Forecast');
+var fiveDayForecastFirstEl = $('#fiveDay-ForecastFirst');
+var fiveDayForecastSecondEl = $('#fiveDay-ForecastSecond');
+var currentDayWeatherEl = $('#currentDayForecast');
 var weatherFormEl = $('#search-city');
 
+
+for (var i = 0; i < localStorage.length; i++) {
+  $('body').append(localStorage.getItem(localStorage.key(i)));
+
+    }
 
 
 function handleFormSubmit(event) {
@@ -8,7 +15,7 @@ function handleFormSubmit(event) {
   var citySearch = $('input[name="city-input"]').val();
   console.log(citySearch);
 
-
+  localStorage.setItem(citySearch, citySearch);
 
 var fetchURLDay = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&units=imperial&appid=5225e5b4c0f8a976d70f50d5260b61e9"
 
@@ -17,9 +24,26 @@ fetch(fetchURLDay)
       return response.json();
     })
     .then(function (data) {
-      console.log('Twitter Public Members: Raw data \n----------');
+      console.log('Current day weather \n----------');
       console.log(data);
 
+      
+          var currentDayTemp = data.main.temp;
+          var currentDayCity = data.name;
+          var currentDayIcon = data.clouds.all;
+          var currentDayHumidity = data.main.humidity;
+          var currentDayWind = data.wind.speed;
+          console.log("this is current day temp: " + currentDayTemp);
+          currentDayWeatherEl.append('<li>' + "Location: " + currentDayCity + '</li>');
+          // currentDayWeatherEl.append('<li>' + currentDayIcon + '</li>');
+          currentDayWeatherEl.append('<li>' + "Current temp: " + currentDayTemp + "F" + '</li>');
+          currentDayWeatherEl.append('<li>' + "Current humidity: " + currentDayHumidity + '</li>');
+          currentDayWeatherEl.append('<li>' + "Wind Speeds: " + currentDayWind + '</li>');
+
+      
+
+
+     
 
     });
 
@@ -31,7 +55,7 @@ fetch(fetchURLDay)
       return response.json();
     })
     .then(function (data) {
-      console.log('Twitter Public Members: Raw data \n----------');
+      console.log('5-day forcast \n----------');
       console.log(data);
 
       for (var i = 0; i < data.list.length; i++) {
@@ -46,13 +70,25 @@ fetch(fetchURLDay)
           console.log("Humidity: " + currentHumidity)
           console.log("Wind Speed: " + currentWindSpeed)
           console.log(data.list[i])
-          fiveDayForecastEl.append('<li>' + currentTemp + '</li>');
+          var forecastCard = document.createElement('div');
+          var tempEl = document.createElement('h3');
+          var humEl = document.createElement('p');
+          var windEl = document.createElement('p');
+          forecastCard.classList.add('border', 'p-5');
+          tempEl.textContent = "Temp: " + currentTemp;
+          humEl.textContent = "Humidity: " + currentHumidity
+          windEl.textContent = "Wind Speed: " + currentWindSpeed
+          forecastCard.append(tempEl, humEl, windEl);
 
+      
+          fiveDayForecastFirstEl.append(forecastCard);
+          // fiveDayForecastSecondEl.append('<li>' + "Humidity: " + currentHumidity + '</li>');
+          // fiveDayForecastEl.append('<li>' + "Windspeed: " + currentWindSpeed + '</li>');
+          
         }
 
 
-
-      
+       
       }
 
 
